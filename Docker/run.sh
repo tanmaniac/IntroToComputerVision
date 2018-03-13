f1dfb71275dac4ab34941099e9be77d96b5dab3d
Tanmay Bangalore <tanmaybangalore@gmail.com>
2018-03-12 22:54:53 -0400

    OpenCV2 build is successful but compiling code fails

---
+++ Docker/run.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+REPO="tanmay"
+IMAGE="cudagl"
+TAG="ud810"
+
+if [ ! "$(docker ps -q -f name=${TAG})" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=${TAG})" ]; then
+        # cleanup
+        docker rm ${TAG}
+    fi
+    docker run -it \
+           --runtime=nvidia \
+           --privileged \
+           --name ${TAG} \
+           -e DISPLAY=$DISPLAY \
+           -v /tmp/.X11-unix:/tmp/.X11-unix \
+           -v $(pwd)/../../IntroToComputerVision:/home/$(id -un)/IntroToComputerVision \
+           ${REPO}/${IMAGE}:${TAG} \
+           /bin/bash
+fi
